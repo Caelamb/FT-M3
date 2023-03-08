92c6337b5b418d85d7852515a158d9db450f4f80
Caelmab <barrylamb530@gmail.com>
2023-03-08 09:35:17 -0600

    04 commit

--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -4,39 +4,38 @@ var http = require("http");
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
 const PORT = 3001;
 
-module.exports = http.createServer((req, res) => {
-  console.log(`Server raised in port ${PORT}`);
-
-  if(req.url === "_/api_"){
-    fs.readFile("utils/dogsData.json", (err, data)=>{
-      if(err){
-        res.writeHead(404, { "Content-Type" : "text/plain"})
-        res.end("json not found")
-      } else {
-        res.writeHead(200, { "Content-Type" : "application/json"});
-        res.end(data)
-      }
-    })
-    return;
-  }
-  if(req.url === "_/allDogs_"){
-    fs.readFile(`utils/allDogs.html`, `UTF8`, (err, data) => {
-      if(err){
-        res.writeHead(404, { "Content-Type" : "text/plain" })
-        res.end("html not found")
-      } else {
-        res.writeHead(200, { "Content-Type" : "text/html"})
-        res.end(data)
-      }
-    })
-    return;
-  } 
-  res.writeHead(404, { 'Content-Type': 'text/plain' });
-  res.end('Route not found');
-  
-}).listen(PORT, "localhost");
-
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
-  
+module.exports =
   /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
   
+  http.createServer((req, res) => {
+    console.log(`Server raised in port ${PORT}`);
+  
+    if(req.url === "/api"){
+      fs.readFile("./utils/dogsData.json", (err, data)=>{
+        if(err){
+          res.writeHead(404, { "Content-Type" : "text/plain"})
+          res.end("json not found")
+        } else {
+          res.writeHead(200, { "Content-Type" : "application/json"});
+          res.end(data)
+        }
+      })
+      return;
+    }
+    if(req.url === "/allDogs"){
+      fs.readFile(`./utils/allDogs.html`, `UTF8`, (err, data) => {
+        if(err){
+          res.writeHead(404, { "Content-Type" : "text/plain" })
+          res.end("html not found")
+        } else {
+          res.writeHead(200, { "Content-Type" : "text/html"})
+          res.end(data)
+        }
+      })
+      return;
+    } 
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Route not found');
+    
+  }).listen(PORT, "localhost");
